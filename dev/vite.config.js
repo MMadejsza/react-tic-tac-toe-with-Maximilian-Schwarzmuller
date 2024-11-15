183f6a71f00aaa727d58b0607729db5eb5986986
Maciej Madejsza <maciej.madejsza@gmail.com>
2024-11-15 12:06:01 +0000

    .

--- dev/vite.config.js
+++ dev/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
 	plugins: [react()],
 	build: {
-		outDir: '../',
+		outDir: '../www',
 		emptyOutDir: true, // Clear folder before build
 	},
 });
